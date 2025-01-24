45a7fa2164295386cae10d90939d60928f85c955
icdada <2045440378@qq.com>
2025-01-24 23:46:06 +0800

    11

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Define the lunch choices
+COMMON_LUNCH_CHOICES += \
+    omni_PD2148-eng \
+    omni_PD2148-user \
+    omni_PD2148-userdebug
